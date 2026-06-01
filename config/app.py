b86924cd7e0dccb59e53ac4bec2ca9f80fbcddc9
hckerson <hckerson@gmail.com>
2026-06-01 11:13:26 +0100

    Refactor app configuration and initialization; add VSCode settings for auto-import completions

--- config/app.py
+++ config/app.py
@@ -1,14 +1,21 @@
 import os
 
+
 class AppConfig:
     FILE_PATH = os.path.abspath(__file__)
     ROOT_DIR = os.path.dirname(os.path.dirname(FILE_PATH))
 
     OUTPUT_DIR = os.path.join(ROOT_DIR, "data", "output")
     INPUT_DIR = os.path.join(ROOT_DIR, "data", "input")
-    print(ROOT_DIR)
-    pass
+
+    def ensure_directories(self):
+        [
+            os.makedirs(dir_path, exist_ok=True)
+            for dir_path in [self.OUTPUT_DIR, self.INPUT_DIR]
+        ]
 
 
 def get_app_config() -> AppConfig:
-    return AppConfig()
+    config = AppConfig()
+    config.ensure_directories()
+    return config
--- src/app.py
+++ src/app.py
@@ -1,1 +1,4 @@
+from config.app import get_app_config
+
+get_app_config()
 print("hello")
